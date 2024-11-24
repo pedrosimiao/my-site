d4fc8a4e74068c89f13c068b09181c3330d6df38
pedrosimiao <pedrolumiere@gmail.com>
2024-11-23 23:15:56 -0300

    Project Update - models,views & urls

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=160)  # Coluna para o título do post
+    content = models.TextField()  # Coluna para o conteúdo do post
+    created_at = models.DateTimeField(auto_now_add=True)  # Data de criação
+
+    def __str__(self):
+        return self.title  # Exibe o título do post como representação
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
-# Create your views here.
+def home(request):
+    # Busca todos os posts do banco de dados
+    posts = Post.objects.all()
+    # Cria uma resposta simples
+    output = "<h1>Blog Posts:</h1>"
+    for post in posts:
+        output += f"<h2><em>{post.title}</em>: {post.content}</h2>"
+    return HttpResponse(output)
+
